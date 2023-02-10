aafc8d567efcbc1e201fd5ea1247a4caf7fd34a2
Rashid Carlos Jamil Marcano-Rivera <67828343+RashidCJ@users.noreply.github.com>
2023-02-10 17:15:10 -0500

    Add files via upload
    intiail commit

---
+++ hello.py
@@ -0,0 +1,6 @@
+#The most basic of commands:
+def basichello(name, greeting):
+    print("Hello Northwestern! %s here. This one comes from a function defined separately in a script. %s"
+          %(name,greeting))
+    
+basichello("Rashid","Happy to be here!")
